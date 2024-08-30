89a7c6ec859346afe3dc23431008edcc50b1f834
Chathurya Dharmasiri <154308126+ChathuDharmasiri@users.noreply.github.com>
2024-08-30 08:24:30 +0530

    Update test program

--- AE07-T-L-TEST CODES/GSM-AE07-COMPLETE/GSM-AE07-COMPLETE.ino
+++ AE07-T-L-TEST CODES/GSM-AE07-COMPLETE/GSM-AE07-COMPLETE.ino
@@ -1,97 +1,104 @@
+
 /*
- * Test Program for NORVI IIOT V2.0
- * Use appropriate libraries supplied.
- * All Outputs should be on when turned on Supplying power to inputs should turn on Relays.
- * Display shows ADC values from Buit-in buttons. Pressing Built-in buttons should change Value displayed on Display 
+ * AE07-GSM-SIM800 R/T
+ * RS485
+ * SIM800C
+ * All Output Turn ON Series
+ * All input status serial print
+ * 
+  Turns ON All Outputs in series
+  Serial prints all the input status
+  SIM800C External Antenna Test
  */
 
-#include <Adafruit_ADS1X15.h>
+#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-Adafruit_ADS1115 ads1;
-Adafruit_ADS1115 ads2;
+#include "FS.h"
+#include <Adafruit_GFX.h>
 
 #define ANALOG_PIN_0 36
 
-#define INPUT1 34
-#define INPUT2 35
-#define INPUT3 14
-#define INPUT4 13
-#define INPUT5 5
-#define INPUT6 15
-#define INPUT7 19
+#define INPUT1 35
+#define INPUT2 34
+#define INPUT3 21
+#define INPUT4 14
+#define INPUT5 13
+#define INPUT6 4
+#define INPUT7 15
+#define INPUT8 26
 
 #define OUTPUT1 12
 #define OUTPUT2 2
 #define OUTPUT3 27
-#define OUTPUT4 4
+#define OUTPUT4 5
 #define OUTPUT5 23
-#define OUTPUT6 18
+#define OUTPUT6 18 
 
 #define RS485_RX 25
-#define RS485_TX 26
+#define RS485_TX 19
 #define RS485_FC 22
 
-#define GSM_RX 32
-#define GSM_TX 33
-#define GSM_RESET 21
+#define GSM_RX 33
+#define GSM_TX 32
 
-unsigned long int timer1 = 0;
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 int analog_value = 0;
-int reading[8]={0};
-String adcString[8];
   
 int readSwitch(){
-  analog_value = analogRead(ANALOG_PIN_0);
+  analog_value = analogRead(ANALOG_PIN_0); 
   return analog_value; //Read analog
 }
+
+unsigned long int timer1 = 0;
+
 // ================================================ SETUP ================================================
-void setup() {  
+void setup() {
   Serial.begin(115200);
+
   Serial.println("Hello");
   
   pinMode(RS485_FC, OUTPUT);
   digitalWrite(RS485_FC, HIGH);   // RS-485 
-  
+    
   Serial1.begin(9600, SERIAL_8N1, RS485_RX, RS485_TX); 
-  Serial2.begin(115200, SERIAL_8N1, GSM_RX, GSM_TX); 
-
-  pinMode(GSM_RESET, OUTPUT);
-  digitalWrite(GSM_RESET, HIGH);    
-
-  Wire.begin(16,17);
-  delay(100);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-  }
-
-  display.display();
-  
+  Serial2.begin(9600, SERIAL_8N1, GSM_TX, GSM_RX); 
+   
   pinMode(OUTPUT1, OUTPUT);
   pinMode(OUTPUT2, OUTPUT);
   pinMode(OUTPUT3, OUTPUT);
   pinMode(OUTPUT4, OUTPUT);
   pinMode(OUTPUT5, OUTPUT);
-  pinMode(OUTPUT6, OUTPUT); 
-
+  pinMode(OUTPUT6, OUTPUT);
+  
   pinMode(INPUT1, INPUT);
   pinMode(INPUT2, INPUT);
   pinMode(INPUT3, INPUT);
   pinMode(INPUT4, INPUT);
   pinMode(INPUT5, INPUT);
   pinMode(INPUT6, INPUT);
-  pinMode(INPUT7, INPUT);  
-    
-  adcAttachPin(36);
+  pinMode(INPUT7, INPUT);
+  pinMode(INPUT8, INPUT);
+  
+  Wire.begin(16,17);
+  Serial.println("Initializing DIsplay");
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.display();
+  
+  Serial.println("Testing Modem");
+  
   timer1 = millis();
   Serial2.println("AT");
-  while(millis()<(timer1+10000)){
+  while(millis()<timer1+10000){
     while (Serial2.available()) {
     int inByte = Serial2.read();
     Serial.write(inByte);
@@ -100,16 +107,29 @@ void setup() {
 
   timer1 = millis();
   Serial2.println("AT+CPIN?");
-  while(millis()<(timer1+10000)){
+  while(millis()<timer1+10000){
     while (Serial2.available()) {
     int inByte = Serial2.read();
     Serial.write(inByte);
     }
   }
+  timer1 = millis();
+  Serial2.println("AT+CFUN?");
+  while(millis()<timer1+10000){
+    while (Serial2.available()) {
+    int inByte = Serial2.read();
+    Serial.write(inByte);
+    }
+  }
+
+  Serial.println("Testing Modem Done");
+  adcAttachPin(36);
+
+  digitalWrite(RS485_FC, HIGH);   // RS-485 
 }
 
-void loop() {
-  // read from port 0, send to port 1:
+void loop() {  
+  // put your main code here, to run repeatedly:
   while (Serial.available()) {
     int inByte = Serial.read();
     Serial2.write(inByte);
@@ -119,7 +139,7 @@ void loop() {
     int inByte = Serial2.read();
     Serial.write(inByte);
   }
-  
+ 
   Serial.print(digitalRead(INPUT1));
   Serial.print(digitalRead(INPUT2));
   Serial.print(digitalRead(INPUT3));
@@ -127,62 +147,48 @@ void loop() {
   Serial.print(digitalRead(INPUT5));
   Serial.print(digitalRead(INPUT6));
   Serial.print(digitalRead(INPUT7));
+  Serial.print(digitalRead(INPUT8));
   Serial.println(""); 
 
   Serial.println(""); 
   Serial.print("Push button  ");
   Serial.println(readSwitch());
   Serial.println(""); 
-    
+  
   digitalWrite(OUTPUT1, HIGH);
   digitalWrite(OUTPUT2, LOW);
   digitalWrite(OUTPUT3, LOW);
   digitalWrite(OUTPUT4, LOW);
-  digitalWrite(OUTPUT5, LOW);
-  digitalWrite(OUTPUT6, LOW);
-  delay(500);
+  delay(200);
   digitalWrite(OUTPUT1, LOW);
   digitalWrite(OUTPUT2, HIGH);
-  digitalWrite(OUTPUT3, LOW);
-  digitalWrite(OUTPUT4, LOW);
-  digitalWrite(OUTPUT5, LOW);
-  digitalWrite(OUTPUT6, LOW);
-  delay(500);
-  digitalWrite(OUTPUT1, LOW);
+  delay(200);
   digitalWrite(OUTPUT2, LOW);
   digitalWrite(OUTPUT3, HIGH);
-  digitalWrite(OUTPUT4, LOW);
-  digitalWrite(OUTPUT5, LOW);
-  digitalWrite(OUTPUT6, LOW);
-  delay(500);
-  digitalWrite(OUTPUT1, LOW);
-  digitalWrite(OUTPUT2, LOW);
+  delay(200);
   digitalWrite(OUTPUT3, LOW);
   digitalWrite(OUTPUT4, HIGH);
-  digitalWrite(OUTPUT5, LOW);
-  digitalWrite(OUTPUT6, LOW);
-  delay(500);
-  digitalWrite(OUTPUT1, LOW);
-  digitalWrite(OUTPUT2, LOW);
-  digitalWrite(OUTPUT3, LOW);
+  delay(200);
   digitalWrite(OUTPUT4, LOW);
   digitalWrite(OUTPUT5, HIGH);
-  digitalWrite(OUTPUT6, LOW);
-  delay(500);
-  digitalWrite(OUTPUT1, LOW);
-  digitalWrite(OUTPUT2, LOW);
-  digitalWrite(OUTPUT3, LOW);
-  digitalWrite(OUTPUT4, LOW);
+  delay(200);
   digitalWrite(OUTPUT5, LOW);
   digitalWrite(OUTPUT6, HIGH);
-  delay(500);
-  digitalWrite(OUTPUT1, LOW);
-  digitalWrite(OUTPUT2, LOW);
-  digitalWrite(OUTPUT3, LOW);
-  digitalWrite(OUTPUT4, LOW);
-  digitalWrite(OUTPUT5, LOW);
+  delay(200);
   digitalWrite(OUTPUT6, LOW);
-
-  Serial1.println("Hello RS-485");
-  delay(1000);
+  delay(200);
+  
+  digitalWrite(RS485_FC, HIGH);                    // Make FLOW CONTROL pin HIGH
+  delay(500);
+  Serial1.println(F("RS485 01 SUCCESS"));          // Send RS485 SUCCESS serially
+  delay(500);                                      // Wait for transmission of data
+  digitalWrite(RS485_FC, LOW) ;                    // Receiving mode ON                                                   // Serial1.flush() ;
+  delay(500);     
+  
+  while (Serial1.available()) {  // Check if data is available
+    char c = Serial1.read();     // Read data from RS485
+    Serial.write(c);             // Print data on serial monitor
+  }
+ //Serial2.println("AT");
+ delay(500); 
 }
